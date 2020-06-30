689a7c21308dc67b6bcbac7b4f8f748efc366d25
moorekwon <raccoonhj33@gmail.com>
2020-07-01 04:23:32 +0900

    crawling 데이터 접근방식 수정 - requests 사용하여 해당 url로 API 데이터 get

--- app/movies/management/commands/dummy_datas.py
+++ app/movies/management/commands/dummy_datas.py
@@ -1,13 +1,9 @@
-import json
-
 from django.core.management import BaseCommand
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        f = open('crawling/jsons/boxoffice_daily.json', 'r')
-        boxoffice_daily_content = json.load(f)
-
-        # 일별박스오피스 랭크 2위인 영화이름 출력
-        rank = 2
-        print(boxoffice_daily_content['boxOfficeResult']['dailyBoxOfficeList'][rank - 1]['movieNm'])
+        # 필요 정보
+        # 영화 정보 -> 개봉일, 영화 이름(국문), 영화 이름(영문), 영화 코드, 상영시간, 장르, 관람등급, 줄거리, 포스터, 예고
+        # 박스오피스 정보 -> 순위, 누적관객수, 예매율
+        pass
