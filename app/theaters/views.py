e7b2a8087e655d8ceb8eb34d90a8ae8dfd8326c2
qu3vipon <sdh5813@gmail.com>
2020-07-02 07:47:06 +0900

    fix: 상영일에 맞는 상영 정보 출력

--- app/theaters/views.py
+++ app/theaters/views.py
@@ -17,15 +17,15 @@ class ScheduleList(ListAPIView):
         date = datetime.strptime(str(date_int), '%y%m%d')
 
         queryset = Schedule.objects.filter(
-            start_time__gte=date,
+            start_time__date=date,
             screen__theater_id=theater_id,
         )
 
-        time = self.request.query_params.get('time', None)
-
-        if time is not None:
-            time = datetime.strptime(str(time), '%H')
-            queryset = queryset.filter(start_time__time__gte=time)
+        # time = self.request.query_params.get('time', None)
+        #
+        # if time is not None:
+        #     time = datetime.strptime(str(time), '%H')
+        #     queryset = queryset.filter(start_time__time__gte=time)
         return queryset
 
 
@@ -35,7 +35,7 @@ class ScheduleTheaterList(ListAPIView):
     def get_queryset(self):
         date_int = self.kwargs['date']
         date = datetime.strptime(str(date_int), '%y%m%d')
-        return Theater.objects.filter(screens__schedules__start_time__gte=date)
+        return Theater.objects.filter(screens__schedules__start_time__date=date).distinct('id')
 
 
 class ScheduleRegionCount(ListAPIView):
@@ -44,7 +44,9 @@ class ScheduleRegionCount(ListAPIView):
     def get_queryset(self):
         date_int = self.kwargs['date']
         date = datetime.strptime(str(date_int), '%y%m%d')
+
+        # 중복된 상영관 존재
         return Theater.objects \
-            .filter(screens__schedules__start_time__gte=date) \
+            .filter(screens__schedules__start_time__date=date) \
             .values('region', 'region__name') \
-            .annotate(region_count=Count('region'))
+            .annotate(region_count=Count('screens'))
