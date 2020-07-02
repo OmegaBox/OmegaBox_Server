f3c9b09499a8fdd77902be340cb40e3e4c65eb95
qu3vipon <sdh5813@gmail.com>
2020-07-03 01:26:00 +0900

    feat: 배포 환경 예외처리를 위한 exception handler 추가

---
+++ app/utils/excepts.py
@@ -0,0 +1,31 @@
+from django.core.exceptions import ValidationError as DjangoValidationError
+from rest_framework import status
+from rest_framework.exceptions import APIException, ValidationError as DRFValidationError
+from rest_framework.views import exception_handler
+
+from config import settings
+
+
+def rest_exception_handler(exc, context):
+    if isinstance(exc, DjangoValidationError):
+        if not settings.DEBUG:
+            raise exc
+        if hasattr(exc, 'message_dict'):
+            exc = DRFValidationError(detail={'error': exc.message_dict})
+        elif hasattr(exc, 'message'):
+            exc = DRFValidationError(detail={'error': exc.message})
+        elif hasattr(exc, 'messages'):
+            exc = DRFValidationError(detail={'error': exc.messages})
+
+    response = exception_handler(exc, context)
+
+    if response:
+        response.data['status'] = response.status_code
+        response.data['code'] = getattr(exc, 'code', getattr(exc, 'default_code', None)) or response.data['detail'].code
+    return response
+
+
+class TakenNumberException(APIException):
+    status_code = status.HTTP_400_BAD_REQUEST
+    default_detail = '이미 가입된 번호입니다.'
+    default_code = 'TakenNumber'
