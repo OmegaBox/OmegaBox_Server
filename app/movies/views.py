860697e1c435537c135eba32d67182ec66f7c2fd
moorekwon <raccoonhj33@gmail.com>
2020-07-08 00:47:56 +0900

    movies AgeBookingSerializer 작업

--- app/movies/views.py
+++ app/movies/views.py
@@ -1,11 +1,9 @@
 from django.db.models import Case, When, Value, Q, CharField, Count, Subquery
-from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, ListAPIView
 from rest_framework.permissions import IsAdminUser, IsAuthenticatedOrReadOnly, AllowAny
-from rest_framework.response import Response
-from rest_framework.views import APIView
 
 from .models import Movie
-from .serializers import MovieSerializer, MovieDetailSerializer
+from .serializers import MovieSerializer, MovieDetailSerializer, AgeBookingSerializer
 
 
 class MovieListView(ListCreateAPIView):
@@ -20,17 +18,41 @@ class MovieDetailView(RetrieveAPIView):
     permission_classes = [IsAdminUser, IsAuthenticatedOrReadOnly, ]
 
 
-class AgeBookingCount(APIView):
-    def get(self, request, pk):
-        movie = Movie.objects.get(pk=pk)
-        teens = movie.schedules.filter(
-            reservations__member__birth_date__year__gt=2000
-        ).filter(
-            reservations__member__birth_date__year__lt=2010
+# 수정 필요
+class AgeBookingView(ListAPIView):
+    serializer_class = AgeBookingSerializer
+    permission_classes = [AllowAny, ]
+
+    def get_queryset(self):
+        queryset = Movie.objects.filter(
+            pk=self.kwargs['pk']
+        ).values(
+            'schedules__reservations__member__id'
+        ).annotate(
+            teens=Count(
+                'schedules__reservations__member',
+                filter=Q(schedules__reservations__member__birth_date__year__lt=2010) &
+                       Q(schedules__reservations__member__birth_date__year__gt=2000),
+            ),
+            twenties=Count(
+                'schedules__reservations__member',
+                filter=Q(schedules__reservations__member__birth_date__year__lt=2000) &
+                       Q(schedules__reservations__member__birth_date__year__gt=1990),
+            ),
+            thirties=Count(
+                'schedules__reservations__member',
+                filter=Q(schedules__reservations__member__birth_date__year__lt=1990) &
+                       Q(schedules__reservations__member__birth_date__year__gt=1980),
+            ),
+            fourties=Count(
+                'schedules__reservations__member',
+                filter=Q(schedules__reservations__member__birth_date__year__lt=1980) &
+                       Q(schedules__reservations__member__birth_date__year__gt=1970),
+            ),
+            fifties=Count(
+                'schedules__reservations__member',
+                filter=Q(schedules__reservations__member__birth_date__year__lt=1970) &
+                       Q(schedules__reservations__member__birth_date__year__gt=1960),
+            ),
         )
-        return Response({
-            '10': teens.count(),
-            # '20': twenties.count(),
-            # '30': thirties.count(),
-            # '40': fourties.count()
-        })
+        return queryset
