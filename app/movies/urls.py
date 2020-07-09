e2448c2651338da3de986fb439bb18e1e7341387
moorekwon <raccoonhj33@gmail.com>
2020-07-09 23:58:53 +0900

    movies 영화별 상세정보 api, 나이별 예매수 정보 api 엔드포인트 변경

--- app/movies/urls.py
+++ app/movies/urls.py
@@ -4,6 +4,6 @@ from .views import MovieListView, MovieDetailView, AgeBookingView
 
 urlpatterns = [
     path('', MovieListView.as_view()),
-    path('<int:pk>/', MovieDetailView.as_view()),
-    path('<int:pk>/age-booking/', AgeBookingView.as_view()),
+    path('detail/<int:pk>/', MovieDetailView.as_view()),
+    path('detail/<int:pk>/age-booking/', AgeBookingView.as_view()),
 ]
