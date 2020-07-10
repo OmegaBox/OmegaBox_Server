40f834dde97edfb7fa17d6900ee6635280f4f0eb
qu3vipon <sdh5813@gmail.com>
2020-07-10 13:27:05 +0900

    list serializer를 통한 다수 좌석 일괄 예매

--- app/reservations/views.py
+++ app/reservations/views.py
@@ -10,14 +10,19 @@ from theaters.models import SeatGrade
 @method_decorator(name='post', decorator=swagger_auto_schema(
     operation_summary='Make Reservation',
     operation_description='영화 좌석 예매 - 결제 전',
-    responses={200: SeatGradeDetailSerializer()}
+    responses={200: SeatGradeDetailSerializer(many=True)}
 ))
 class SeatGradeCreateView(CreateAPIView):
     queryset = SeatGrade
     serializer_class = SeatGradeCreateSerializer
     permission_classes = [IsAuthenticated, ]
 
+    def get_serializer(self, *args, **kwargs):
+        kwargs['many'] = True
+        return super(SeatGradeCreateView, self).get_serializer(*args, **kwargs)
+
     def perform_create(self, serializer):
-        instance = serializer.save()
-        instance.reservation.member = self.request.user
-        instance.reservation.save()
+        instance_list = serializer.save()
+        for instance in instance_list:
+            instance.reservation.member = self.request.user
+            instance.reservation.save()
